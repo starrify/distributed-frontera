f24a73b24b7e6c96603b2c11b1100695e470dd0e
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-10-06 20:08:25 +0200

    First working version of 0MQ-based messagebus.

--- distributed_frontera/messagebus/zeromq.py
+++ distributed_frontera/messagebus/zeromq.py
@@ -5,6 +5,7 @@ from distributed_frontera.worker.partitioner import FingerprintPartitioner, Crc3
 import zmq
 from time import time, sleep
 from struct import pack
+import six
 
 
 class Consumer(BaseStreamConsumer):
@@ -34,26 +35,27 @@ class Producer(object):
         self.sender = context.socket(zmq.PUB)
         self.sender.bind(location)
 
-    def send(self, message, key):
+    def send(self, key, *messages):
+        # Guarantee that msg is actually a list or tuple (should always be true)
+        if not isinstance(messages, (list, tuple)):
+            raise TypeError("msg is not a list or tuple!")
+
+        # Raise TypeError if any message is not encoded as bytes
+        if any(not isinstance(m, six.binary_type) for m in messages):
+            raise TypeError("all produce message payloads must be type bytes")
+
         partition = self.partitioner.partition(key)
-        self.sender.send_multipart([pack(">B", partition), message])
+        for msg in messages:
+            self.sender.send_multipart([pack(">B", partition), msg])
+
+    def flush(self):
+        pass
 
 
 class SpiderLogProducer(Producer):
     def __init__(self, context, location, partitions):
         super(SpiderLogProducer, self).__init__(context, location)
         self.partitioner = FingerprintPartitioner(partitions)
-
-
-class UpdateScoreProducer(Producer):
-    def send(self, message, key):
-        self.sender.send(message)
-
-
-class SpiderFeedProducer(Producer):
-    def __init__(self, context, location, partitions):
-        super(SpiderFeedProducer, self).__init__(context, location)
-        self.partitioner = Crc32NamePartitioner(partitions)
 
 
 class SpiderLogStream(BaseSpiderLogStream):
@@ -69,6 +71,19 @@ class SpiderLogStream(BaseSpiderLogStream):
         return Consumer(self.context, self.location, partition_id)
 
 
+class UpdateScoreProducer(Producer):
+    def send(self, key, *messages):
+        # Guarantee that msg is actually a list or tuple (should always be true)
+        if not isinstance(messages, (list, tuple)):
+            raise TypeError("msg is not a list or tuple!")
+
+        # Raise TypeError if any message is not encoded as bytes
+        if any(not isinstance(m, six.binary_type) for m in messages):
+            raise TypeError("all produce message payloads must be type bytes")
+        for msg in messages:
+            self.sender.send_multipart(['', msg])
+
+
 class UpdateScoreStream(BaseUpdateScoreStream):
     def __init__(self, messagebus):
         self.context = messagebus.context
@@ -81,24 +96,10 @@ class UpdateScoreStream(BaseUpdateScoreStream):
         return UpdateScoreProducer(self.context, self.location)
 
 
-class SpiderFeedConsumer(BaseStreamConsumer):
-    def __init__(self, context, location, partition_id):
-        self.subscriber = context.socket(zmq.REP)
-        self.subscriber.connect(location)
-
-    def get_messages(self, timeout=0.1, count=1):
-        started = time()
-        while count:
-            try:
-                msg = self.subscriber.recv_multipart(copy=True, flags=zmq.NOBLOCK)
-            except zmq.Again:
-                sleep(0.01)
-                if time() - started > timeout:
-                    break
-            else:
-                yield msg[1]
-                count -= 1
-
+class SpiderFeedProducer(Producer):
+    def __init__(self, context, location, partitions):
+        super(SpiderFeedProducer, self).__init__(context, location)
+        self.partitioner = Crc32NamePartitioner(partitions)
 
 
 class SpiderFeedStream(BaseSpiderFeedStream):
@@ -108,13 +109,13 @@ class SpiderFeedStream(BaseSpiderFeedStream):
         self.partitions = messagebus.spider_feed_partitions
 
     def consumer(self, partition_id):
-        pass
+        return Consumer(self.context, self.location, partition_id)
 
     def producer(self):
-        pass
+        return SpiderFeedProducer(self.context, self.location, self.partitions)
 
     def available_partitions(self):
-        pass
+        return self.partitions
 
 
 class MessageBus(BaseMessageBus):
@@ -123,10 +124,10 @@ class MessageBus(BaseMessageBus):
 
         # FIXME: Options!
         self.spider_log_location = "tcp://127.0.0.1:5551"
-        self.spider_log_partitions = [i for i in range(2)]
+        self.spider_log_partitions = [i for i in range(1)]
         self.update_score_location = "tcp://127.0.0.1:5552"
         self.spider_feed_location = "tcp://127.0.0.1:5553"
-        self.spider_feed_partitions = [i for i in range(2)]
+        self.spider_feed_partitions = [i for i in range(1)]
 
     def spider_log(self):
         return SpiderLogStream(self)
